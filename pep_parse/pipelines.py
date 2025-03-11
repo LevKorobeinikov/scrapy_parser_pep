a99015fcb06075d773601b7a69006223de6e9ac5
LevKorobeinikov <korobeinikoffl@yandex.ru>
2025-03-11 15:13:07 +0500

    fix

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -15,42 +15,42 @@ COUNT = 'Количество'
 TOTAL = 'ИТОГО'
 
 
-class Base:
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
-    id = Column(Integer, primary_key=True)
+# class Base:
+#     @declared_attr
+#     def __tablename__(cls):
+#         return cls.__name__.lower()
+#     id = Column(Integer, primary_key=True)
 
 
-Base = declarative_base(cls=Base)
+# Base = declarative_base(cls=Base)
 
 
-class Pep(Base):
-    status = Column(Text)
-    number = Column(Integer)
-    name = Column(String(200))
+# class Pep(Base):
+#     status = Column(Text)
+#     number = Column(Integer)
+#     name = Column(String(200))
 
 
-class PepsToDBPipeline:
+# class PepsToDBPipeline:
 
-    def open_spider(self, spider):
-        engine = create_engine('sqlite:///sqlite.db')
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(engine)
-        self.session = Session()
+#     def open_spider(self, spider):
+#         engine = create_engine('sqlite:///sqlite.db')
+#         Base.metadata.create_all(engine)
+#         Session = sessionmaker(engine)
+#         self.session = Session()
 
-    def process_item(self, item, spider):
-        peps = Pep(
-            number=item[NUMBER_PEP],
-            name=item[NAME],
-            status=item[STATUS_PEP],
-        )
-        self.session.add(peps)
-        self.session.commit()
-        return item
+#     def process_item(self, item, spider):
+#         peps = Pep(
+#             number=item[NUMBER_PEP],
+#             name=item[NAME],
+#             status=item[STATUS_PEP],
+#         )
+#         self.session.add(peps)
+#         self.session.commit()
+#         return item
 
-    def close_spider(self, spider):
-        self.session.close()
+#     def close_spider(self, spider):
+#         self.session.close()
 
 
 class PepParsePipeline:
